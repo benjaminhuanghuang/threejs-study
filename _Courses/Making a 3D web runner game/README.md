26965e39a6c7509c9776d08bc9986efb1e3224a9
Benjamin Huang <benjhuang@gmail.com>
2024-01-21 10:00:39 -0800

    update

--- _Courses/Making a 3D web runner game/README.md
+++ _Courses/Making a 3D web runner game/README.md
@@ -1,13 +1,20 @@
 # Making a 3D web runner game
 
-https://www.youtube.com/watch?v=TsmhYonWyZ4&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=12
+<https://www.youtube.com/watch?v=TsmhYonWyZ4&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=12>
 
 1. Create a basic 3D Scene
-https://www.youtube.com/watch?v=zklHTcaGV44&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=13&pp=iAQB
-
+<https://www.youtube.com/watch?v=zklHTcaGV44&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=13&pp=iAQB>
 
 2. Design Game class
-https://www.youtube.com/watch?v=TsmhYonWyZ4&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=12
+<https://www.youtube.com/watch?v=TsmhYonWyZ4&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=12>
 
 3. Modelling the spaceship
-https://www.youtube.com/watch?v=PhqWAcWnjvc&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=11
+<https://www.youtube.com/watch?v=PhqWAcWnjvc&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=11>
+
+Ship is a group
+
+4. Making an infinite plane with a shader
+<https://www.youtube.com/watch?v=sQZLNuU7jWM&list=PLbEtFK5YvXKU2UhV1_mXlb21LlB7HdPjJ&index=10>
+
+
+5. 
--- _Courses/Making a 3D web runner game/hyperspeed-tuto/game.js
+++ _Courses/Making a 3D web runner game/hyperspeed-tuto/game.js
@@ -43,11 +43,83 @@ export default class Game {
 
   initializeScene(scene, camera) {
     // prepare 3D scene
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh(geometry, material);
-    scene.add(this.cube);
+    const shipBody = new THREE.Mesh(
+      new THREE.TetrahedronBufferGeometry(0.4),
+      new THREE.MeshBasicMaterial({ color: 0xbbccdd })
+    );
+    shipBody.rotateX((45 * Math.PI) / 180);
+    shipBody.rotateY((45 * Math.PI) / 180);
+
+    this.ship = new THREE.Group();
+    this.ship.add(shipBody);
+
+    scene.add(this.ship);
 
     camera.position.z = 5;
+    camera.rotateX((-20 * Math.PI) / 180);
+    camera.position.set(0, 1.5, 2);
+
+    const reactorSocketGeometry = new THREE.CylinderBufferGeometry(
+      0.08,
+      0.08,
+      0.1,
+      16
+    );
+    const reactorSocketMaterial = new THREE.MeshBasicMaterial({
+      color: 0x99aacc,
+    });
+    const reactorSocket1 = new THREE.Mesh(
+      reactorSocketGeometry,
+      reactorSocketMaterial
+    );
+    const reactorSocket2 = new THREE.Mesh(
+      reactorSocketGeometry,
+      reactorSocketMaterial
+    );
+    const reactorSocket3 = new THREE.Mesh(
+      reactorSocketGeometry,
+      reactorSocketMaterial
+    );
+
+    this.ship.add(reactorSocket1);
+    this.ship.add(reactorSocket2);
+    this.ship.add(reactorSocket3);
+    reactorSocket1.rotateX((90 * Math.PI) / 180);
+    reactorSocket1.position.set(-0.15, 0, 0.1);
+    reactorSocket2.rotateX((90 * Math.PI) / 180);
+    reactorSocket2.position.set(0.15, 0, 0.1);
+    reactorSocket3.rotateX((90 * Math.PI) / 180);
+    reactorSocket3.position.set(0, -0.15, 0.1);
+
+    const reactorLightGeometry = new THREE.CylinderBufferGeometry(
+      0.055,
+      0.055,
+      0.1,
+      16
+    );
+    const reactorLightMaterial = new THREE.MeshBasicMaterial({
+      color: 0xaadeff,
+    });
+    const reactorLight1 = new THREE.Mesh(
+      reactorLightGeometry,
+      reactorLightMaterial
+    );
+    const reactorLight2 = new THREE.Mesh(
+      reactorLightGeometry,
+      reactorLightMaterial
+    );
+    const reactorLight3 = new THREE.Mesh(
+      reactorLightGeometry,
+      reactorLightMaterial
+    );
+    this.ship.add(reactorLight1);
+    this.ship.add(reactorLight2);
+    this.ship.add(reactorLight3);
+    reactorLight1.rotateX((90 * Math.PI) / 180);
+    reactorLight1.position.set(-0.15, 0, 0.11);
+    reactorLight2.rotateX((90 * Math.PI) / 180);
+    reactorLight2.position.set(0.15, 0, 0.11);
+    reactorLight3.rotateX((90 * Math.PI) / 180);
+    reactorLight3.position.set(0, -0.15, 0.11);
   }
 }
